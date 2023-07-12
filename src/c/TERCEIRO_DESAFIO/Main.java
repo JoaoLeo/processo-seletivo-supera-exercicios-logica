2d7b9565a22a95cf8dcc240afb83c070d46c7a91
João Leonardo <joaoleonardo9921@gmail.com>
2023-07-12 19:50:10 -0300

    adicionando terceiro desafio

--- src/c/TERCEIRO_DESAFIO/Main.java
+++ src/c/TERCEIRO_DESAFIO/Main.java
@@ -1,38 +1,58 @@
 package c.TERCEIRO_DESAFIO;
-/*
-Dado um array de inteiros e um valor alvo, determine o número de pares entre os elementos
-do array em que a sua diferença seja igual ao valor alvo.
 
-Exemplo
-K = 1
-arr = [1,2,3,4]
-Existem 3 valores cuja diferença é igual ao valor alvo K: 2-1 = 1, 3-2 = 1, 4-3 = 1.
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
 
-Descrição do problema:
-Os pares têm os seguintes parâmetros:
-int k: Um Inteiro, valor alvo.
-int arr[n]: Um array de Inteiros.
-
-Retorno
-int: O número de pares que satisfazem o critério.
-
-Formatos de Entrada
-Seu código deve conter duas entradas n e k, que representam o tamanho do array e o valor
-alvo.
-Seu código deve conter um array de inteiros, de tamanho n.
-
-Exemplos de entrada
-
-STDIN Function
------ --------
-5 2 arr[] tamanho n = 5, k =2
-1 5 3 4 2 arr = [1, 5, 3, 4, 2]
-
-Exemplo de saida
-3
- */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner in = new Scanner(System.in);
+        List<Integer> arr = new ArrayList<>();
+        
+        System.out.println("Dado um array de inteiros e um valor alvo, determine o número de pares entre os elementos\n" +
+                "do array em que a sua diferença seja igual ao valor alvo.");
+        System.out.print("Digite o tamanho do array: ");
+        int sizeArray = in.nextInt();
+
+        if(sizeArray <= 0 ) {
+            System.out.println("Digite um valor valido");
+            return;
+        }
+
+        for (int i = 0; i < sizeArray ; i++) {
+            System.out.print((i + 1) + "° elemento do array: ");
+            int elemento = in.nextInt();
+            arr.add(elemento);
+
+        }
+        System.out.print("O array cadastrado foi: [ ");
+        arr.forEach(a -> System.out.print(a + " "));
+        System.out.println("]");
+        System.out.print("Agora digite o valor alvo: ");
+        int k = in.nextInt();
+        int qtd = valorDiferencaDePares(arr, k);
+        System.out.println("Existem " + qtd +" valores cuja diferença é igual ao valor alvo");
+        in.close();
+    }
+
+    public static int valorDiferencaDePares(List<Integer> arr, int k) {
+        Collections.sort(arr);
+        int qtd = 0;
+        int i = 0;
+        int j = 1;
+
+        while (j < arr.size()) {
+            int diff = arr.get(j) - arr.get(i);
+            if (diff == k) {
+                qtd++;
+                j++;
+            } else if (diff > k) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+        return qtd;
     }
 }
